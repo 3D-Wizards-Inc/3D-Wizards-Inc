32374131d4149d1c3787004adc868267a7b22acd
DrM00G <68446064+DrM00G@users.noreply.github.com>
2024-12-15 21:41:48 -1000

    Custom Orders Table added to Admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,7 @@ import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import React from 'react';
+// import CustomOrder from '../custom/page';
 
 const AdminPage = async () => {
   const session = await getServerSession(authOptions);
@@ -18,6 +19,7 @@ const AdminPage = async () => {
   const products = await prisma.product.findMany({
     where: { checkedout: true },
   });
+  const customs = await prisma.custom_Orders.findMany({});
 
   return (
     <main>
@@ -25,13 +27,14 @@ const AdminPage = async () => {
         <Row>
           <Col>
             <h1 className="pt-5">Checked out user products</h1>
-            <hr style={{
-              border: 'none',
-              height: '5px',
-              background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
-              margin: '25px 0',
-              width: '900px',
-            }}
+            <hr
+              style={{
+                border: 'none',
+                height: '5px',
+                background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
+                margin: '25px 0',
+                width: '900px',
+              }}
             />
             <Table striped bordered hover>
               <thead>
@@ -67,23 +70,25 @@ const AdminPage = async () => {
             </Table>
           </Col>
         </Row>
-        <hr style={{
-          border: 'none',
-          height: '2px',
-          background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
-          margin: '25px 0',
-        }}
+        <hr
+          style={{
+            border: 'none',
+            height: '2px',
+            background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
+            margin: '25px 0',
+          }}
         />
         <Row>
           <Col>
             <h1 className="pt-5">Registered users list</h1>
-            <hr style={{
-              border: 'none',
-              height: '5px',
-              background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
-              margin: '25px 0',
-              width: '900px',
-            }}
+            <hr
+              style={{
+                border: 'none',
+                height: '5px',
+                background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
+                margin: '25px 0',
+                width: '900px',
+              }}
             />
             <Table striped bordered hover>
               <thead>
@@ -98,6 +103,63 @@ const AdminPage = async () => {
                     <td>{user.email}</td>
                     <td>{user.role}</td>
                   </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <hr
+          style={{
+            border: 'none',
+            height: '2px',
+            background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
+            margin: '25px 0',
+          }}
+        />
+        <Row>
+          <Col>
+            <h1 className="pt-5">Custom Orders</h1>
+            <hr
+              style={{
+                border: 'none',
+                height: '5px',
+                background: 'linear-gradient(to right, #ddd, #bbb, #ddd)',
+                margin: '25px 0',
+                width: '900px',
+              }}
+            />
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Mat 1</th>
+                  <th>Mat 2</th>
+                  <th>Mat 3</th>
+                  <th>Type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <React.Fragment key={user.id}>
+                    {/* inserts row per user in the database */}
+                    <tr>
+                      <td colSpan={8}>
+                        <strong>{user.email}</strong>
+                      </td>
+                    </tr>
+                    {/* should display all products where the owner fits the user.email property of above */}
+                    {customs
+                      .filter((custom) => custom.user === user.email) // product filtering by email
+                      .map((custom) => (
+                        <tr key={custom.id}>
+                          <td>{custom.description}</td>
+                          <td>{custom.material1}</td>
+                          <td>{custom.material2}</td>
+                          <td>{custom.material3}</td>
+                          <td>{custom.type}</td>
+                        </tr>
+                      ))}
+                  </React.Fragment>
                 ))}
               </tbody>
             </Table>
