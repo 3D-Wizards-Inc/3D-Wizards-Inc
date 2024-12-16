84b8476256d94abea126620e71f515c46490b639
Minkyeong Kim <kim83@hawaii.edu>
2024-12-15 22:45:42 -1000

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -30,37 +30,44 @@ const AboutUsPage = async () => (
           <Row className="mb-4">
             <Col className="d-flex align-items-start">
               <Image src="/images/Tom.JPG" alt="Tom" width="200" height="300" className="me-3" />
-              <h4 className="mb-1">Tom</h4>
-              <p className="mb-0">
-                Team Lead, Head of Product Design, Print Farm Manager
-              </p>
-            </Col>
-          </Row>
-          <Row className="mb-4">
-            <Col className="d-flex align-items-start">
-              <Image src="/images/Aaron.JPG" alt="Aaron" width="200" height="300" className="me-3" />
-              <div>
-                <h4 className="mb-1">Aaron</h4>
-                <p className="mb-0">Retail and Business Relations Manager</p>
+              <div className="mt-3">
+                <h4 className="mb-1">Tom</h4>
+                <p className="mb-0">
+                  Team Lead, Head of Product Design, Print Farm Manager
+                </p>
               </div>
             </Col>
           </Row>
           <Row className="mb-4">
             <Col className="d-flex align-items-start">
+              <Image src="/images/Aaron.JPG" alt="Aaron" width="200" height="300" className="me-3" />
+              <div className="mt-3">
+                <h4 className="mb-1">Aaron</h4>
+                <p className="mb-0">Retail and Business Relations Manager</p>
+              </div>
+            </Col>
+
+          </Row>
+          <Row className="mb-4">
+            <Col className="d-flex align-items-start">
               <Image src="/images/Jesse.JPG" alt="Jesse" width="200" height="300" className="me-3" />
-              <h4 className="mb-1">Jesse</h4>
-              <p className="mb-0">
-                Employee Manager
-              </p>
+              <div className="mt-3">
+                <h4 className="mb-1">Jesse</h4>
+                <p className="mb-0">
+                  Employee Manager
+                </p>
+              </div>
             </Col>
           </Row>
           <Row className="mb-4">
             <Col className="d-flex align-items-start">
               <Image src="/images/David.JPG" alt="David" width="200" height="300" className="me-3" />
-              <h4 className="mb-1">David</h4>
-              <p className="mb-0">
-                Retail Associate and Printer technician
-              </p>
+              <div className="mt-3">
+                <h4 className="mb-1">David</h4>
+                <p className="mb-0">
+                  Retail Associate and Printer technician
+                </p>
+              </div>
             </Col>
           </Row>
         </Col>
